77ac77fb8b25220fbd00fe87ee3183aa7746380a
tuuvv <vuvantuu@gmail.com>
2022-04-08 23:31:53 +0700

    fix CSS àn link the title to url post in detail popup

--- views/ALstatistics.php
+++ views/ALstatistics.php
@@ -108,11 +108,11 @@ error_reporting(E_ERROR | E_PARSE);
     <style>
 #incoming_View{
     background-color:#fff;
-    width:1200px;
-    height:900px;
+    width:60%;
+    height:100%;
     display:block;
     position:fixed;
-    top:20%;
+    top:30%;
     left:30%;
     margin:-150px 0 0 -150px;
 }
@@ -142,11 +142,11 @@ error_reporting(E_ERROR | E_PARSE);
     <style>
 #outgoingView{
     background-color:#fff;
-    width:1200px;
-    height:900px;
+    width:60%;
+    height:100%;
     display:block;
     position:fixed;
-    top:20%;
+    top:30%;
     left:30%;
     margin:-150px 0 0 -150px;
 }
@@ -180,7 +180,7 @@ error_reporting(E_ERROR | E_PARSE);
     <?php foreach($data1 as $item): if( $item < 10):?>
 
     <tr>
-        <td><?php echo get_the_title($item->link_to);?></td>
+        <td><a href="<?php echo get_permalink($item->link_to);?>"><?php echo get_the_title($item->link_to);?></a></td>
         <td><?php echo get_cat_name(wp_get_post_categories($item->link_to)[0]);?></td>  
     </tr>
 
@@ -208,7 +208,7 @@ error_reporting(E_ERROR | E_PARSE);
     <?php foreach($data2 as $item): ?>
 
     <tr>
-        <td><?php echo get_the_title($item->link_from);?></td>
+    <td><a href="<?php echo get_permalink($item->link_from);?>"><?php echo get_the_title($item->link_from);?></a></td>
         <td><?php echo get_cat_name(wp_get_post_categories($item->link_from)[0]);?></td>  
     </tr>
 
@@ -218,6 +218,7 @@ error_reporting(E_ERROR | E_PARSE);
     </table> 
     
 </div>
+
 <!--///////////////////////////////////////////////////////-->
   <script>
        jQuery(document).ready(function() {
